15606e486641ab946fbe84e3279e9f32f9020094
Nico <nico.zimmermann@it.htl-hl.ac.at>
2024-10-15 14:16:00 +0200

    included tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Home from "./Views/Home";
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Views/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Components/MainComponents/Navbar";
+import HeroSection from "../Components/HeroComponents/HeroSection";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <HeroSection />
+    </div>
+  );
+}
+
+export default Home;
