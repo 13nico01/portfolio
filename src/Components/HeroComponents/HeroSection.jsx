e9b1c628473e49b00f48a7b4df367434859c4dff
Nico <nico.zimmermann@it.htl-hl.ac.at>
2024-10-16 09:13:50 +0200

    Navbar added

--- src/Components/HeroComponents/HeroSection.jsx
+++ src/Components/HeroComponents/HeroSection.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function HeroSection() {
   return (
-    <div>
-      <h1 className="text-center py-8 text-4xl">Hello World</h1>
+    <div className=''>
+      <h1 className="text-center py-8 text-7xl font-mono">Welcome to Roomman.at</h1>
     </div>
   )
 }
--- src/Components/MainComponents/Navbar.jsx
+++ src/Components/MainComponents/Navbar.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Menu, X } from 'lucide-react';
 
-function Navbar() {
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <nav className="flex flex-col md:flex-row justify-between items-center p-4 bg-gradient-to-r from-orange-500 to-orange-700">
+      <div className="flex items-center justify-between w-full md:w-auto">
+        <a href="#" className="text-lg font-bold text-white">
+          Roomman
+        </a>
+        <button 
+          className="md:hidden text-white focus:outline-none" 
+          onClick={toggleMenu}
+        >
+          {isOpen ? <X/> : <Menu/>}
+        </button>
+      </div>
+      <ul className={`flex-col md:flex-row md:flex md:items-center ${isOpen ? 'flex' : 'hidden'} md:block`}>
+        <li className="mr-6">
+          <a href="#" className="text-white hover:text-gray-900 transition duration-500">
+            Home
+          </a>
+        </li>
+        <li className="mr-6">
+          <a href="#" className="text-white hover:text-gray-900 transition duration-500">
+            About
+          </a>
+        </li>
+        <li className="mr-6">
+          <a href="#" className="text-white hover:text-gray-900 transition duration-500">
+            Contact
+          </a>
+        </li>
+      </ul>
+      <div className={`flex-col md:flex-row md:flex md:items-center ${isOpen ? 'flex' : 'hidden'} md:block`}>
+        <button className="bg-transparent hover:bg-gray-600 transition duration-700 text-white font-bold py-2 px-4 rounded ">
+          Sign Up
+        </button>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
