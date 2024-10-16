ed252b8b4feedc7f9ec1fb179ddb4d6a09b2f021
Nico <nico.zimmermann@it.htl-hl.ac.at>
2024-10-16 11:24:07 +0200

    google analytics

--- src/Components/HeroComponents/HeroSection.jsx
+++ src/Components/HeroComponents/HeroSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function HeroSection() {
   return (
     <div className=''>
-      <h1 className="text-center py-8 text-7xl font-mono">Welcome to Roomman.at</h1>
+      <h1 className="text-center py-8 text-7xl  font-mono">Welcome to Roomman.at</h1>
     </div>
   )
 }
