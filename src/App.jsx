40d16c97b0668f610049ff20d71306e561ea07f9
Nico <nico.zimmermann@it.htl-hl.ac.at>
2024-12-16 19:31:43 +0100

    error handling

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ import Contact from "./Components/Contact";
 
 const App = () => {
   return (
-    <div className="text-neutral-200 antialiased overflow-x-hidden ">
-      <div className="fixed w-full h-full -z-10 top-0 ">
-        <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+    <div className="text-neutral-200 antialiased flex items-center justify-center overflow-hidden">
+      <div className="fixed top-0 left-0 w-screen h-screen -z-10 overflow-hidden">
+        <div className="absolute inset-0 -z-10 h-full py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
       </div>
-      <div className="container mx-2 lg:mx-auto px-8">
+      <div className=" container mx-auto max-w-screen-lg p-4">
         <Navbar />
         <Hero />
         <About />
